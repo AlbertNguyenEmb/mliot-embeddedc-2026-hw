edab773107f94e099f7aa1967ebff09fe402ec93
Nhat Nguyen <albert.nguyentechemb@gmail.com>
2026-06-26 22:45:30 +0700

    week01: hoan thanh bai tap buoi 1

--- week01/main.c
+++ week01/main.c
@@ -7,10 +7,11 @@
 
 void parse_config(const uint8_t *config_packet, int16_t *high_threshold) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
+    if (config_packet == NULL || high_threshold == NULL) {
+        return;
+    }
 
-
-
-
+    *high_threshold = (int16_t)((config_packet[1]) << 8 | config_packet[0]);
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
 
@@ -18,9 +19,13 @@ void parse_config(const uint8_t *config_packet, int16_t *high_threshold) {
 
 int16_t read_temperature_reg(void *hw_sensor_reg) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
+    if (hw_sensor_reg == NULL) {
+        return 0;
+    }
 
+    volatile int16_t *sensor_reg = (volatile int16_t *)hw_sensor_reg;
 
-
+    return *sensor_reg;
 
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
@@ -30,9 +35,21 @@ int16_t read_temperature_reg(void *hw_sensor_reg) {
 void control_output(uint8_t *control_reg, uint8_t fan_enable, uint8_t alarm_enable) {
     // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
 
+    if (control_reg == NULL) {
+        return;
+    }
 
+    if (fan_enable) {
+        *control_reg |= (1 << 0);
+    } else {
+        *control_reg &= ~(1 << 0);
+    }
 
-
+    if (alarm_enable) {
+        *control_reg |= (1 << 1);
+    } else {
+        *control_reg &= ~(1 << 1);
+    }
     // HỌC VIÊN KẾT THÚC VIẾT CODE
 }
 
